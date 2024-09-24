8b6d3547d561290eb6d4381f8bc668609cb40179
SumitRaj <rajsumit1793@gmail.com>
2024-09-24 20:24:10 +0530

    deploy to ec2

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ import colors from "colors";
 
 const connectDB = async () => {
   try {
-    const con = await mongoose.connect(process.env.MONGO_URL);
+    const password = process.env.PASSWORD;
+    const con = await mongoose.connect(`mongodb+srv://rajsumit1793:${password}@cluster1.8ixsrpo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`);
     console.log(
       `Connected to Database Successfully ${con.connection.host}`.bgBlue.white
     );
